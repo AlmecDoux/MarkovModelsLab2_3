26113965cb11d9e38a138a7070f16005a949cbcd
a_fedin <a.fedin@sendy.land>
2021-12-28 11:11:46 +0300

    Удалено лишнее

--- main.py
+++ main.py
@@ -8,12 +8,6 @@ mtrx = np.array([[0.9, 0.1], [0.2, 0.8]])
 v = [-1, 1]
 m = 3
 q = 3
-d = {
-    (1, 1): 0,
-    (1, -1): 0,
-    (-1, 1): 0,
-    (-1, -1): 0,
-}
 
 
 # Генерация цепи Маркова и получение матрицы вероятности переходов
@@ -26,15 +20,13 @@ def generate_next_value_chain(x):
 
 
 def generate_chain(N):
-    chain = []
+    chain_ = []
     next_value = np.random.choice(v, p=start_property)
-    chain.append(next_value)
+    chain_.append(next_value)
     for _ in np.arange(0, N - 1):
-        tmp = next_value
         next_value = generate_next_value_chain(next_value)
-        d[(tmp, next_value)] += 1
-        chain.append(next_value)
-    return chain
+        chain_.append(next_value)
+    return chain_
 
 
 def generate_psp(psp_array):
@@ -47,62 +39,14 @@ def generate_psp(psp_array):
 
 
 chain = generate_chain(N)
-count_one = len(list(filter(lambda x: x == 1, chain)))
-count_m_one = len(list(filter(lambda x: x == -1, chain)))
-p_matrix = np.array([
-    [d[(1, 1)] / count_one, d[(1, -1)] / count_one],
-    [d[(-1, 1)] / count_m_one, d[(-1, -1)] / count_m_one]
-])
 
-# Получение ПСП
-psp_array = generate_psp([1, 1, 1])
-L = 2 ** m - 1
-psp_array = psp_array[:L]
-
-
-def add_noise(chain_, psp_array_):
-    noise_chain = []
-    for chain_char in chain_:
-        for noise_char in psp_array_:
-            print('noise_char', noise_char)
-            print('chain_char', chain_char)
-            print(noise_char + chain_char)
-            noise_chain.append((noise_char + chain_char) % 2)
-    return noise_chain
-
-
-def delete_noise(noise_chain, psp_):
-    clean_chain = []
-    k = 0
-    for noise_chain_part in noise_chain:
-        if k == len(psp_):
-            k = 0
-        print(k)
-        clean_chain.append((noise_chain_part + psp_[k]) % 2)
-        k = k + 1
-    return clean_chain
 
 def D(m1, m2):
     return m1 - m2
 
-def normal_form(chain):
-    for i in range(0, len(chain)):
-        if chain[i] == 0:
-            chain[i] = 1
-        else:
-            chain[i] = -1
-    return chain
 
-def signal_transformation(s):
-    if s == v[0]:
-        return 1
-    else:
-        return -1
-
-
-def phasing_manipulation(s):
-    return 4 * q * (signal_transformation(s) + np.random.uniform(0, 1, 1) / (pow(2 * q, 1 / 2)))
-
+def FM_phasing_manipulation(signal, q, gauss):
+    return 4 * q * (signal_transformation(signal) + np.random.uniform(0, 1, 1) / (pow(2 * q, 1 / 2)))
 
 
 chain = normal_form(chain)
